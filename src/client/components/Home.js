6b6f8a1f3023b3f159bcb93dc83263996bba5564
Alexander Steele <alexsteele110@gmail.com>
2017-11-16 20:27:55 -0500

    added client bundle.js

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <div>I am the VERY BEST home component</div>
+      <div>I am the BEST home component</div>
       <button onClick={() => console.log('Hi there!')}>Press me</button>
     </div>
   );
