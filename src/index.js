024dff484c42a74529eb3f5cfefa4834de488ccb
Alexander Steele <alexsteele110@gmail.com>
2017-11-20 19:53:10 -0500

    added userList component and refactored routing

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
+import 'babel-polyfill';
 import express from 'express';
+import { matchRoutes } from 'react-router-config';
+import Routes from './client/Routes';
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
 
@@ -8,7 +11,7 @@ app.use(express.static('public'));
 app.get('*', (req, res) => {
   const store = createStore();
 
-  // Some logic to initialize and load data into store
+  matchRoutes(Routes, req.path);
 
   res.send(renderer(req, store));
 });
