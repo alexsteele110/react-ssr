8b3eab33d2d2ec81548f5c5ddd76217d55929478
Alexander Steele <alexsteele110@gmail.com>
2017-12-12 19:18:36 -0500

    added adminsListPage along with action and reducer to handle it

--- src/client/actions/index.js
+++ src/client/actions/index.js
@@ -1,5 +1,4 @@
 export const FETCH_USERS = 'fetch_users';
-
 export const fetchUsers = () => async (dispatch, getState, api) => {
   const res = await api.get('/users');
 
@@ -7,9 +6,15 @@ export const fetchUsers = () => async (dispatch, getState, api) => {
 };
 
 export const FETCH_CURRENT_USER = 'fetch-current-user';
-
 export const fetchCurrentUser = () => async (dispatch, getState, api) => {
   const res = await api.get('/current_user');
 
   dispatch({ type: FETCH_CURRENT_USER, payload: res });
+};
+
+export const FETCH_ADMINS = 'fetch-admins';
+export const fetchAdmins = () => async (dispatch, getState, api) => {
+  const res = await api.get('/admins');
+
+  dispatch({ type: FETCH_ADMINS, payload: res });
 };
